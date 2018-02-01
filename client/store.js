650f422980d075a788340e4ed103672c81669c5d
Ola Vea <ola@olavea.com>
2018-02-01 07:23:53 +0100

    Update store.js

--- client/store.js
+++ client/store.js
@@ -3,4 +3,8 @@
 //create an object for the default data
 const defaultState = { posts };
 
-const store = createStore();
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
